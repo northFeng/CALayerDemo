eb655a796600ea6ce134104eeb1a954da06fa0c9
峰 <north_feng@sina.cn>
2020-04-13 17:54:36 +0800

    优化

--- CALayer/GFUIViewAnimation.m
+++ CALayer/GFUIViewAnimation.m
@@ -130,7 +130,39 @@
     } completion:^(BOOL finished) {
         
     }];
-    
+    /**
+      1.常规动画属性设置（可以同时选择多个进行设置）
+         UIViewAnimationOptionLayoutSubviews：动画过程中保证子视图跟随运动。**提交动画的时候布局子控件，表示子控件将和父控件一同动画。**
+         UIViewAnimationOptionAllowUserInteraction：动画过程中允许用户交互。
+         UIViewAnimationOptionBeginFromCurrentState：所有视图从当前状态开始运行。
+         UIViewAnimationOptionRepeat：重复运行动画。
+         UIViewAnimationOptionAutoreverse ：动画运行到结束点后仍然以动画方式回到初始点。**执行动画回路,前提是设置动画无限重复**
+         UIViewAnimationOptionOverrideInheritedDuration：忽略嵌套动画时间设置。**忽略外层动画嵌套的时间变化曲线**
+         UIViewAnimationOptionOverrideInheritedCurve：忽略嵌套动画速度设置。**通过改变属性和重绘实现动画效果，如果key没有提交动画将使用快照**
+         UIViewAnimationOptionAllowAnimatedContent：动画过程中重绘视图（注意仅仅适用于转场动画）。
+         UIViewAnimationOptionShowHideTransitionViews：视图切换时直接隐藏旧视图、显示新视图，而不是将旧视图从父视图移除（仅仅适用于转场动画）**用显隐的方式替代添加移除图层的动画效果**
+         UIViewAnimationOptionOverrideInheritedOptions ：不继承父动画设置或动画类型。**忽略嵌套继承的�选项**
+         ----------------------------------------------------------------------------
+         2.动画速度控制（可从其中选择一个设置）时间函数曲线相关**时间曲线函数**
+         UIViewAnimationOptionCurveEaseInOut：动画先缓慢，然后逐渐加速。
+         UIViewAnimationOptionCurveEaseIn ：动画逐渐变慢。
+         UIViewAnimationOptionCurveEaseOut：动画逐渐加速。
+         UIViewAnimationOptionCurveLinear ：动画匀速执行，默认值。
+         -----------------------------------------------------------------------------
+         3.转场类型（仅适用于转场动画设置，可以从中选择一个进行设置，基本动画、关键帧动画不需要设置）**转场动画相关的**
+         UIViewAnimationOptionTransitionNone：没有转场动画效果。
+         UIViewAnimationOptionTransitionFlipFromLeft ：从左侧翻转效果。
+         UIViewAnimationOptionTransitionFlipFromRight：从右侧翻转效果。
+         UIViewAnimationOptionTransitionCurlUp：向后翻页的动画过渡效果。
+         UIViewAnimationOptionTransitionCurlDown ：向前翻页的动画过渡效果。
+         UIViewAnimationOptionTransitionCrossDissolve：旧视图溶解消失显示下一个新视图的效果。
+         UIViewAnimationOptionTransitionFlipFromTop ：从上方翻转效果。
+         UIViewAnimationOptionTransitionFlipFromBottom：从底部翻转效果。
+
+         补充：关于最后一组转场动画它一般是用在这个方法中的：
+     　　　　[UIView transitionFromView: toView: duration: options:  completion:^(****BOOL****finished) {}];
+        该方法效果是插入一面视图移除一面视图，期间可以使用一些转场动画效果。
+     */
 }
 
 
